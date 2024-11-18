652d05597c822dff758a672775c82fd5fe2cd49f
Amit Neumann <amit.neumann@post.runi.ac.il>
2024-11-18 22:21:39 +0200

    try4

--- CalcPi.java
+++ CalcPi.java
@@ -3,7 +3,7 @@ public class CalcPi {
 	public static void main(String [] args) { 
 		int n=Integer.parseInt(args[0]),denominator;
 		double sum=0.0, numerator;
-		for(int i=0;i<n&&i!=0;i++) {
+		for(int i=0;i<n;i++) {
 			if(i%2==0) {
 				numerator=1.0;
 			} else {
@@ -14,7 +14,7 @@ public class CalcPi {
 		}
 		System.out.println("Pi according to Java: "+Math.PI);
 		if (n!=0) {
-			System.out.println("Pi, approximated:	 "+sum*4);
+			System.out.println("Pi, approximated:     "+sum*4);
 		} else 
 			System.out.println("Pi, approximated: cannot be done with n=0");
 	}
--- Cheers.java
+++ Cheers.java
@@ -5,35 +5,19 @@ public class Cheers {
          int rows=Integer.parseInt(args[1]);
          name=name.toUpperCase();
          for (int i=0;i<name.length();i++) {
-                if(specialLetters.indexOf(name.charAt(i))==-1){ // if my letter is a letter in specialletters-word
-                  System.out.println("Give me a "+name.charAt(i)+": "+name.charAt(i)+"!");
+                if(specialLetters.indexOf(name.charAt(i))==-1){ 
+                  System.out.println("Give me a  "+name.charAt(i)+": "+name.charAt(i)+"!");
                 } else {
-                        System.out.println("Give me an "+name.charAt(i)+": "+name.charAt(i)+"!");
+                        System.out.println("Give me an  "+name.charAt(i)+": "+name.charAt(i)+"!");
                         }
          } System.out.println("What does that spell?");
-         for (int j=0;j<rows&&rows!=0;j++) {
+         for (int j=0;j<rows;j++) {
                 System.out.println(name+"!!!");
          }
    }
         
  }
 
-
- // for(int i=0; i<name.length();i++) { // going over letters of name
-// chr=name.charAt(i);
- //isLike=false;
- //for(int j=0;j<specialLetters.length();j++) { // בודקת אות מהמילה מול כל אות אסורה
-   //      if (chr==specialLetters.charAt(j))
-     //            isLike=true;
- //}  if (isLike) {
-   //      System.out.println("Give me an "+chr+": "+chr+"!");
- //} else {
-   //      System.out.println("Give me a "+chr+": "+chr+"!");
- //}
-//} System.out.println("What does that spell?");
-//for (int k=0;k<rows;k++) {
- //System.out.println(name+"!!!");
-        
-        
+       
         
        
