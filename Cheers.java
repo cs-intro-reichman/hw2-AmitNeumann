01a1c6939f2e465df92875b4251ad089aa9cf2ea
Amit Neumann <amit.neumann@post.runi.ac.il>
2024-11-18 23:19:14 +0200

    cheers without space

--- Cheers.java
+++ Cheers.java
@@ -7,13 +7,14 @@ public class Cheers {
          for (int i=0;i<name.length();i++) {
               if(Character.isLetter(name.charAt(i))){
                 if(specialLetters.indexOf(name.charAt(i))==-1){ 
-                        System.out.println("Give me a  "+name.charAt(i)+": "+name.charAt(i)+"!");
+                        System.out.println("Give me a "+name.charAt(i)+": "+name.charAt(i)+"!");
                       } else {
-                              System.out.println("Give me an  "+name.charAt(i)+": "+name.charAt(i)+"!");
+                              System.out.println("Give me an "+name.charAt(i)+": "+name.charAt(i)+"!");
                               } 
                 finalName=finalName+name.charAt(i);              
               }
          } System.out.println("What does that spell?");
+         finalName=finalName.toUpperCase();
          for (int j=0;j<rows;j++) {
                 System.out.println(finalName+"!!!");
          }
