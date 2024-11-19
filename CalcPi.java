c34dc70fff338b25b37f556e17e425ef966c509c
Amit Neumann <amit.neumann@post.runi.ac.il>
2024-11-19 13:25:39 +0200

    tuesday try

--- CalcPi.java
+++ CalcPi.java
@@ -12,10 +12,8 @@ public class CalcPi {
 			denominator=1+(i*2);
 			sum=sum+(numerator/denominator);
 		}
-		System.out.printf("Pi according to Java: %.15f%n", Math.PI);
-		if (n>0) {
-			System.out.printf("Pi, approximated:     %.15f%n", sum * 4);
-		} else 
-			System.out.println("Pi, approximated: cannot be done with n=0");
+		System.out.println("pi according to Java: "+ Math.PI);
+		System.out.println("pi, approximated:     "+ sum * 4);
+
 	}
 }
